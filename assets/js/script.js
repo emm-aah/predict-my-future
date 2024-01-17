43cc232e48839d57ad786bbfa58e47b8841ddb27
emm-aah <>
2024-01-17 11:49:39 +0000

    add new html in Javascript for Question 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,44 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener("click", function() {
             let chosenAnswer = this.getAttribute("id")
             if (this.getAttribute("data-type") === "answer") {
-                document.getElementById("question").innerText = `You picked ${chosenAnswer}!`;
+                let emptyBox= document.getElementById("reveal-answer")
+                .innerText = `You picked ${chosenAnswer}!`;
+            } else if (this.getAttribute("data-type") === "submit"){
+                submitAnswer();
+                nextQuestion();
             } else {
                 alert(`You need to chose an answer`);
             }
         });
     }
-    
 });
-/** 
-function chooseAnswer(chosenAnswer){
-     document.getElementById(reveal-answer).innerText = `You have picked ${chosenAnswer}`
-    }
 
 function submitAnswer(chosenAnswer){
-     if (chosenAnswer === "blue") {
-        incremenentPredictionOne();
-     }
+    if (chosenAnswer === "blue") {
+        incrementPredictionOne();
+    } else if (chosenAnswer === "orange") {
+        console.log("orange");
+    } else if (chosenAnswer === "turquoise") {
+        console.log("turquoise");
+    } else {
+        console.log("pink");
+    }
 }
-function nextQuestion()
+function nextQuestion() {
+    document.getElementById("question-area").innerHTML = `<h3 id="question">Pick a food</h3>
+            <div class="color-answers">
+            <button class="btn answer-button pasta" id="pasta" data-type="answer" >Pasta</button>
+            <button class="btn answer-button sushi" id="sushi" data-type="answer">Sushi</button>
+            <button class="btn answer-button roast" id="roast" data-type="answer">Roast</button>
+            <button class="btn answer-button curry" id="curry" data-type="answer">Curry</button>
+            </div> ;`
+}
+/** 
 function incrementPredictionOne() {
     let oldScore = parseInt(document.getElementById("predict-one").innerText);
     document.getElementById("predict-one").innerText = ++oldScore;
 }
+
 function incrementPredictionTwo()
 function incrementPredictionThree()
 function incrementPredictionFour() **/
