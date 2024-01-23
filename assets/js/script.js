2348a2d3da3f2d78c2086cfb667fec13ed583caf
emm-aah <>
2024-01-23 17:35:34 +0000

    add working submit function that increases the prediction score

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,17 +29,45 @@ let questions = [
         ]
     },
     {
-        question: "Pick a First Date",
+        question: "Pick a Sport",
         answers: [
-            { option: "Cinema", value: "3" },
-            { option: "Drinks", value: "4" },
-            { option: "Hike", value: "1" },
-            { option: "Bowling", value: "2" }
+            { option: "Basketball", value: "1" },
+            { option: "Golf", value: "2" },
+            { option: "Tennis", value: "3" },
+            { option: "Football", value: "4" }
+        ]
+    },
+    {
+        question: "Pick a genre of Music",
+        answers: [
+            { option: "Rock", value: "2" },
+            { option: "Pop", value: "4" },
+            { option: "Rap", value: "3" },
+            { option: "Disco", value: "1" }
+        ]
+    },
+    {
+        question: "Pick a Tv Show",
+        answers: [
+            { option: "Game of Thrones", value: "3" },
+            { option: "The Office", value: "2" },
+            { option: "Friends", value: "4" },
+            { option: "The Wire", value: "1" }
+        ]
+    },
+    {
+        question: "Pick a Fizzy Drink",
+        answers: [
+            { option: "Coke", value: "3" },
+            { option: "Fanta", value: "1" },
+            { option: "Sprite", value: "2" },
+            { option: "Pepsi", value: "4" }
         ]
     },
 ]
 
 let currentQuestion = 0
+let predictionScore = 0
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -51,20 +79,51 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let nextBtn = document.getElementById("next");
     let submitBtn = document.getElementById("submit");
-    nextBtn.addEventListener("click", nextQuestion);
-    submitBtn.addEventListener("click", submitAnswer)
+    let predictBtn = document.getElementById("predict")
+    nextBtn.addEventListener("click", function(){
+        submitAnswer();
+        nextQuestion();
+    });
+    predictBtn.addEventListener("click", changePage);      
+
 
 
 });
 
 function submitAnswer() {
-    currentQuestion = currentQuestion + 1
+    let choices = document.getElementsByName("answer-option");
+    let choiceValue;
+
+    for (let i = 0; i < choices.length; i++){
+        if (choices[i].checked) {
+            choiceValue = parseInt(questions[currentQuestion].answers[i].value);
+            predictionScore = predictionScore + choiceValue;
+            currentQuestion = currentQuestion + 1
+        } 
+    }
+    
 }
 
 function nextQuestion() {
+    if (currentQuestion >= 6) {
+        document.getElementById("next").classList.add('hide');
+        document.getElementById("predict").classList.remove('hide');
+    }
     document.getElementById("question").innerText = questions[currentQuestion].question;
     document.getElementById("a").innerText = questions[currentQuestion].answers[0].option;
     document.getElementById("b").innerText = questions[currentQuestion].answers[1].option;
     document.getElementById("c").innerText = questions[currentQuestion].answers[2].option;
     document.getElementById("d").innerText = questions[currentQuestion].answers[3].option;
+}
+
+function changePage() {
+ document.getElementbyId("page").innerHTML =
+  `<section class='prediction-background'>
+        <div id = inside-ball>
+            <p></p>
+        </div>
+
+    </section>`;
+
+
 }
